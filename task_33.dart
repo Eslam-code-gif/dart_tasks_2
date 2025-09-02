c0ac6d354293488ffd37ae680e8e26fb638eb29f
SoLo <eslamlion58@gmail.com>
2025-09-02 22:15:08 +0300

    33

--- task_33.dart
+++ task_33.dart
@@ -1,1 +1,24 @@
-void main(){ }
+import 'dart:io';
+
+void main(){ 
+  List<int> numbers = [];
+
+  for (int i = 1; i <= 5; i++) {
+    stdout.write('Enter number $i: ');
+
+      String? input = stdin.readLineSync();
+      if (input != null && input.isNotEmpty) {
+        numbers.add(int.parse(input));
+      } else {
+        print('Invalid input, please enter a valid integer.');
+        i--;
+      }
+    } 
+    for (var number in numbers) {
+      if (number % 2 == 0) {
+        print('$number is even');
+      } else {
+        print('$number is odd');
+      }
+    }
+}
