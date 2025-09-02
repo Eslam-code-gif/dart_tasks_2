f0f2d2def3eabc7188b235614e75fbc616e49b74
SoLo <eslamlion58@gmail.com>
2025-09-02 22:09:41 +0300

    31

--- task_31.dart
+++ task_31.dart
@@ -1,1 +1,10 @@
-void main(){ }
+import 'dart:io';
+
+void main() {
+  var originalList = List.generate(5, (index) => index, growable: false); 
+  
+//  final actualLength = originalList.length;
+  stdout.write("Enter your guess for the length of the list: ");
+  var userGuess = int.tryParse( stdin.readLineSync() ?? '0') ?? 0; 
+  print("Your guess is ${userGuess == originalList.length ? 'correct' : 'incorrect'}. The actual length is ${originalList.length}.");
+}
