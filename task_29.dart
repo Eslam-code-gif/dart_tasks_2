d85d8c812226a4f35912029e1d69af31f8173af1
SoLo <eslamlion58@gmail.com>
2025-09-02 21:58:42 +0300

    29

--- task_29.dart
+++ task_29.dart
@@ -1,3 +1,18 @@
-void main(){ 
+import 'dart:io';
+
+void main() {
+  // Task 29
+
+  stdout.write('Enter 5 numbers separated by spaces: ');
+  String input = stdin.readLineSync()!;
+
+  List<String> numbersAsString = input.split(' ');
+
+  for (String numberStr in numbersAsString) {
+      int number = int.parse(numberStr);
+      int remainder = number % 3;
+      print('$number remainder 3 is $remainder');
+    
+  }
   
 }
