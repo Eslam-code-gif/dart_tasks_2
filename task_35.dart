52a3f300e76e498dd8c17227d3f5eadbca6514f1
SoLo <eslamlion58@gmail.com>
2025-09-02 22:20:03 +0300

    35

--- task_35.dart
+++ task_35.dart
@@ -1,1 +1,8 @@
-void main(){ }
+void main() {
+  List<int> numbers = [2, 4, 5, 9, 10, 11];
+int m = 1;
+  for (int x in numbers) {
+    m*=x;
+  }
+  print("The result of multiplying all elements: $m");
+}
