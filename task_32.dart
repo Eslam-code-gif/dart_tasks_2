47ab24646d46ae01976155cd62b7a83d36380034
SoLo <eslamlion58@gmail.com>
2025-09-02 22:11:53 +0300

    32

--- task_32.dart
+++ task_32.dart
@@ -1,1 +1,13 @@
-void main(){ }
+import 'dart:io';
+
+void main(){ 
+  stdout.write("Enter your number: ");
+  var x = int.tryParse( stdin.readLineSync() ?? '0') ?? 0; 
+  if(x>0){
+    print("Positive");
+  } else if(x<0){
+    print("Negative");
+  } else {
+    print("Zero");
+  }
+}
