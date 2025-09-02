41b61a9ff97404ea9f2209df24f379f06d5ec6f9
SoLo <eslamlion58@gmail.com>
2025-09-02 22:17:14 +0300

    34

--- task_34.dart
+++ task_34.dart
@@ -1,1 +1,9 @@
-void main(){ }
+void main() {
+  List<int> numbers = [2, 4, 5, 6, 8, 9, 10, 11];
+
+  for (int x in numbers) {
+    if ((x * x) % 2 == 0) {
+      print('$x % 2 = 0');
+    }
+  }
+}
