d1c19cd146c0e566280b13eb59cbd6ff85bb4fa0
SoLo <eslamlion58@gmail.com>
2025-09-02 22:03:15 +0300

    30

--- task_30.dart
+++ task_30.dart
@@ -1,1 +1,28 @@
-void main(){ }
+import 'dart:io';
+
+void main() {
+  List<int> numbers = [];
+
+  for (int i = 1; i <= 8; i++) {
+    stdout.write('Enter number $i: ');
+
+      String? input = stdin.readLineSync();
+      if (input != null && input.isNotEmpty) {
+        numbers.add(int.parse(input));
+      } else {
+        print('Invalid input, please enter a valid integer.');
+        i--;
+      }
+    } 
+
+  List<int> multiplications = [];
+
+  for (int i = 0; i < numbers.length - 1; i++) {
+    int result = numbers[i] * numbers[i + 1];
+    multiplications.add(result);
+  }
+
+  print('The entered numbers: ${numbers.join(', ')}');
+  
+  print('The multiplications numbers: ${multiplications.reversed.join(', ')}');
+}
