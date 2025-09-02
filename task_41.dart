47fe19c0e8af21d07f69ca494af3a2a89a683a4e
SoLo <eslamlion58@gmail.com>
2025-09-02 22:37:43 +0300

    41

--- task_41.dart
+++ task_41.dart
@@ -1,1 +1,10 @@
-void main(){ }
+void main() {
+  final now = DateTime.now();
+
+  print('${now.day.toString().padLeft(2, '0')}/${now.month.toString().padLeft(2, '0')}/${now.year}');
+
+  final dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  print(dayNames[now.weekday - 1]);
+
+  print('${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}');
+}
