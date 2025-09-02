f7561a327ddc155bffe5cfc9b6f4d5ba8c6cae73
SoLo <eslamlion58@gmail.com>
2025-09-02 22:25:24 +0300

    37

--- task_37.dart
+++ task_37.dart
@@ -1,1 +1,20 @@
-void main(){ }
+import 'dart:io';
+
+void main() {
+  Set<int> mySet = {1, 3, 5, 7, 8, 10};
+
+  stdout.write('Enter a number to check: ');
+  String? userInput = stdin.readLineSync();
+
+  int? userNumber = int.tryParse(userInput ?? '');
+
+  if (userNumber != null) {
+    if (mySet.contains(userNumber)) {
+      print('the number you are looking for is found which is $userNumber');
+    } else {
+      print('that number is not found');
+    }
+  } else {
+    print('Invalid input. Please enter a valid number.');
+  }
+}
