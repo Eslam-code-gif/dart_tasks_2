b3f0482b1b837d4546955d898311e8863d888c8b
SoLo <eslamlion58@gmail.com>
2025-09-02 22:23:07 +0300

    36

--- task_36.dart
+++ task_36.dart
@@ -1,1 +1,10 @@
-void main(){ }
+void main() {
+  List<int> numbers = [2, 1, -5, 9, 10, 11];
+var res =[];
+  for (int x in numbers) {
+    if(x*3<8){
+      res.add(x);
+    }
+  }
+  print(res);
+}
